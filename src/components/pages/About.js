41dfd50db1608212633e14fb5bbf25077080d097
Thien Nguyen <Thiennguyenwsb@gmail.com>
2019-02-21 11:12:14 -0800

    final

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <React.Fragment> 
+      <h1>About</h1>
+      <p>this is the TodoList app v1.0.0 it is part of a React crash course</p>
+    </React.Fragment>
+  )
+}
